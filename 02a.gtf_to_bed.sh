2d0497549c20ea7d048829ddfaf7010b8cb46a06
Geok Fong <79799984+geokfong@users.noreply.github.com>
2025-05-08 23:23:33 +0800

    Update 02a.gtf_to_bed.sh

--- 02a.gtf_to_bed.sh
+++ 02a.gtf_to_bed.sh
@@ -1,4 +1,3 @@
-awk -F"\t" '{if(FNR == NR){hash[$2]=$1}else{if($0 !~ /#/){split($9, info, ";"); OFS="\t"; print $1,$2,$3,$4,$5,$6,$7,$8,hash[" "info[1]]"; "$9}}}' ENSG_TRANSID.txt introns.gtf | head
 awk -F"\t" '{if(FNR == NR){hash[$2]=$1}else{if($0 !~ /#/){split($9, info, ";"); OFS="\t"; print $1,$2,$3,$4,$5,$6,$7,$8,hash[" "info[1]]"; "$9}}}' ENSG_TRANSID.txt introns.gtf > Introns_wnames.gtf
 
 cat Introns_wnames.gtf Transcripts_to_work_on.gtf > combined.gtf
