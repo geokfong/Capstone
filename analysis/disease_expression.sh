e0596c39967e10796eb6fb5107a4ff7c3d6f24d6
Geok Fong <79799984+geokfong@users.noreply.github.com>
2025-10-22 00:24:20 +0800

    Refactor disease_expression.sh to remove redundancies
    Removed unnecessary bedtools intersect commands and cleaned up the script.

--- analysis/disease_expression.sh
+++ analysis/disease_expression.sh
@@ -13,7 +13,6 @@ bedtools intersect -a "/Volumes/Lucky me/8_STAMP/29apr_mincov10/metrics_5kb/regi
 -wao \
 > "/Volumes/Lucky me/8_STAMP/29apr_mincov10/metrics_5kb/output_clinvarMain.tsv"
 
-#remove the first column
 cut -f 2- "/Volumes/Lucky me/8_STAMP/bin/04.sample_metrics_final_v3/expression_level/disease_expression/GWAS" \
 > "/Volumes/Lucky me/8_STAMP/bin/04.sample_metrics_final_v3/expression_level/disease_expression/GWAS_update.tsv"
 
@@ -21,26 +20,3 @@ bedtools intersect -a "/Volumes/Lucky me/8_STAMP/29apr_mincov10/metrics_5kb/regi
 -b "/Volumes/Lucky me/8_STAMP/bin/04.sample_metrics_final_v3/expression_level/disease_expression/GWAS_update.tsv" \
 -wao \
 > "/Volumes/Lucky me/8_STAMP/29apr_mincov10/metrics_5kb/output_GWAS.tsv"
-
-bedtools intersect -a "/Volumes/Lucky me/8_STAMP/29apr_mincov10/metrics_5kb/region.bed" \
--b "/Volumes/Lucky me/8_STAMP/bin/04.sample_metrics_final_v3/expression_level/repeat/repeatmasker_class.bed" \
--wao \
-> "/Volumes/Lucky me/8_STAMP/29apr_mincov10/metrics_5kb/output_repeatmasker_class.tsv"
-
-#repeat masker, to get repClass (more general), previous input was downloaded online in bed format, and it was only contain repName (more specific)
-awk 'BEGIN{OFS="\t"} {print $6, $7, $8, $12, $2, $10}' repeatmasker.tsv > repeatmasker_class.bed
-tail -n +2 "/Volumes/Lucky me/8_STAMP/bin/04.sample_metrics_final_v3/expression_level/repeat/repeatmasker_class.bed" \
-| cut -f1-6 \
-> "/Volumes/Lucky me/8_STAMP/bin/04.sample_metrics_final_v3/expression_level/repeat/repeatmasker_class_clean.bed"
-mv repeatmasker_class_clean.bed repeatmasker_class.bed
-
-bedtools intersect \
--a "/Volumes/Lucky me/8_STAMP/29apr_mincov10/metrics_5kb/region.bed" \
--b "/Volumes/Lucky me/8_STAMP/bin/04.sample_metrics_final_v3/expression_level/repeat/repeatmasker_class.bed" \
--wao \
-> "/Volumes/Lucky me/8_STAMP/29apr_mincov10/metrics_5kb/output_repeatmasker_class.tsv"
-
-
-
-
-
